bbe451a529e6d733e123805a8ccc5490988b00aa
Nolimet <nolimet@stamdata.nl>
2016-02-25 14:06:24 +0100

    added some cool editor stuff for the enemy spawner

--- Assets/Scripts/Managers/EnemySpawner.cs
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
-    Vector3 spawnMin;
+    public Vector3 spawnMin;
     [SerializeField]
-    Vector3 spawnMax;
-    
+    public Vector3 spawnMax;
+
     void Start()
     {
 
@@ -16,30 +16,38 @@ public class EnemySpawner : MonoBehaviour
     float t;
     void Update()
     {
-            TestSpawn();
+        TestSpawn();
     }
 
     void TestSpawn()
     {
         GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Destroy(g.GetComponent<BoxCollider>());
+        //Destroy(g.GetComponent<BoxCollider>());
         g.name = "Test";
+        PhysicMaterial pm = new PhysicMaterial("BOUNCE");
+        pm.bounciness = 0.8f;
+        g.GetComponent<BoxCollider>().material = pm;
         g.transform.SetParent(transform, false);
-
         g.transform.localPosition = RandomPos();
 
-        Destroy(g, 3f);
-        
+        Rigidbody r = g.AddComponent<Rigidbody>();
+
+        r.velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        r.useGravity = false;
+
+
+        Destroy(g, 10f);
+
     }
 
     Vector3 RandomPos()
     {
+
         return new Vector3(Random.Range(spawnMin.x, spawnMax.x), Random.Range(spawnMin.y, spawnMax.y), Random.Range(spawnMin.z, spawnMax.z));
     }
 
 
 #if UNITY_EDITOR
-
     Vector3 tmpMax;
     Vector3 tmpMin;
     public void OnDrawGizmosSelected()
@@ -51,7 +59,28 @@ public class EnemySpawner : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawCube(tmpMax, Vector3.one * 0.5f);
 
-        Gizmos.color = Color.yellow;
+        //Gizmos.color = Color.yellow;
+        //Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
+        //Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMax.y, tmpMin.z));
+        //Gizmos.DrawLine(tmpMin, new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
+
+        //Gizmos.DrawLine(tmpMax, new Vector3(tmpMax.x, tmpMax.y, tmpMin.z));
+        //Gizmos.DrawLine(tmpMax, new Vector3(tmpMax.x, tmpMin.y, tmpMax.z));
+        //Gizmos.DrawLine(tmpMax, new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
+
+        //Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMin.y, tmpMax.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
+        //Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
+
+        //Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMax.y, tmpMin.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
+        //Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMax.y, tmpMin.z));
+
+        //Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
+        //Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
         Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
         Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMax.y, tmpMin.z));
         Gizmos.DrawLine(tmpMin, new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
@@ -69,7 +98,6 @@ public class EnemySpawner : MonoBehaviour
         Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
         Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
     }
-
 
 
 #endif
