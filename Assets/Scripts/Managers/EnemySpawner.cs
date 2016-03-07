b9ca330761786afa558c76a65459207799e73c5f
pepi <pepi55@live.com>
2016-03-07 10:21:29 +0100

    Updated Enemyspawner prefab.
    Re imported enemyspawner gameobject in prefab.
    Cleared whitespaces from Enemyspawner.cs.

--- Assets/Scripts/Managers/EnemySpawner.cs
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -3,111 +3,102 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
-    //public for a editor script
-    public Vector3 spawnMin;   
-    public Vector3 spawnMax;
+	//public for a editor script
+	public Vector3 spawnMin;
+	public Vector3 spawnMax;
 
-    [SerializeField]
-    GameObject Enemy;
+	[SerializeField]
+	GameObject Enemy;
 
-    [SerializeField, Tooltip("time in seconds")]
-    float SpawnDelay = 0.05f;
+	[SerializeField, Tooltip("time in seconds")]
+	float SpawnDelay = 0.05f;
 
 #if UNITY_EDITOR
-    [SerializeField]
-    Color lineColor;
+	[SerializeField]
+	Color lineColor;
 #endif
 
-    void Start()
-    {
+	float t;
+	void Update()
+	{
+		if (t > SpawnDelay)
+		{
+			TestSpawn();
+			t = 0;
+		}
 
-    }
+		t += Time.deltaTime;
+	}
 
-    float t;
-    void Update()
-    {
-        if (t > SpawnDelay)
-        {
-            TestSpawn();
-            t = 0;
-        }
+	void TestSpawn()
+	{
+		GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		//Destroy(g.GetComponent<BoxCollider>());
+		g.name = "Test";
+		PhysicMaterial pm = new PhysicMaterial("BOUNCE");
+		pm.bounciness = 0.8f;
+		g.GetComponent<BoxCollider>().material = pm;
+		g.transform.SetParent(transform, false);
+		g.transform.localPosition = RandomPos();
 
-        t += Time.deltaTime;
-    }
+		Rigidbody r = g.AddComponent<Rigidbody>();
 
-    void TestSpawn()
-    {
-        GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //Destroy(g.GetComponent<BoxCollider>());
-        g.name = "Test";
-        PhysicMaterial pm = new PhysicMaterial("BOUNCE");
-        pm.bounciness = 0.8f;
-        g.GetComponent<BoxCollider>().material = pm;
-        g.transform.SetParent(transform, false);
-        g.transform.localPosition = RandomPos();
+		r.velocity = new Vector3(0, 0, -2);
+		r.useGravity = false;
 
-        Rigidbody r = g.AddComponent<Rigidbody>();
+		g.AddComponent<EnemyBase>();
 
-        r.velocity = new Vector3(0, 0, -2);
-        r.useGravity = false;
+		Destroy(g, 20f);
+	}
 
-        g.AddComponent<EnemyBase>();
+	Vector3 RandomPos()
+	{
+		return new Vector3(Random.Range(spawnMin.x, spawnMax.x), Random.Range(spawnMin.y, spawnMax.y), Random.Range(spawnMin.z, spawnMax.z));
+	}
 
-
-        Destroy(g, 20f);
-
-    }
-
-    Vector3 RandomPos()
-    {
-
-        return new Vector3(Random.Range(spawnMin.x, spawnMax.x), Random.Range(spawnMin.y, spawnMax.y), Random.Range(spawnMin.z, spawnMax.z));
-    }
-
-    [ContextMenu("Center")]
-    void Center()
-    {
-        Vector3 startPos = transform.position;
-        transform.position = Vector3.Lerp(spawnMax + transform.position, spawnMin + transform.position, 0.5f);
-        Vector3 diff =transform.position - startPos;
-        spawnMin -= diff;
-        spawnMax -= diff;
-    }
+	[ContextMenu("Center")]
+	void Center()
+	{
+		Vector3 startPos = transform.position;
+		transform.position = Vector3.Lerp(spawnMax + transform.position, spawnMin + transform.position, 0.5f);
+		Vector3 diff =transform.position - startPos;
+		spawnMin -= diff;
+		spawnMax -= diff;
+	}
 
 #if UNITY_EDITOR
-    Vector3 tmpMax;
-    Vector3 tmpMin;
-    public void OnDrawGizmosSelected()
-    {
-        tmpMax = transform.position + spawnMax;
-        tmpMin = transform.position + spawnMin;
-        Gizmos.color = Color.green;
-        Gizmos.DrawCube(tmpMin, Vector3.one * 0.5f);
-        Gizmos.color = Color.red;
-        Gizmos.DrawCube(tmpMax, Vector3.one * 0.5f);
-        
-    }
+	Vector3 tmpMax;
+	Vector3 tmpMin;
+	public void OnDrawGizmosSelected()
+	{
+		tmpMax = transform.position + spawnMax;
+		tmpMin = transform.position + spawnMin;
+		Gizmos.color = Color.green;
+		Gizmos.DrawCube(tmpMin, Vector3.one * 0.5f);
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube(tmpMax, Vector3.one * 0.5f);
+	}
 
-    public void OnDrawGizmos()
-    {
-        Gizmos.color = lineColor;
-        Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
-        Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMax.y, tmpMin.z));
-        Gizmos.DrawLine(tmpMin, new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
+	public void OnDrawGizmos()
+	{
+		Gizmos.color = lineColor;
+		Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
+		Gizmos.DrawLine(tmpMin, new Vector3(tmpMin.x, tmpMax.y, tmpMin.z));
+		Gizmos.DrawLine(tmpMin, new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
 
-        Gizmos.DrawLine(tmpMax, new Vector3(tmpMax.x, tmpMax.y, tmpMin.z));
-        Gizmos.DrawLine(tmpMax, new Vector3(tmpMax.x, tmpMin.y, tmpMax.z));
-        Gizmos.DrawLine(tmpMax, new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
+		Gizmos.DrawLine(tmpMax, new Vector3(tmpMax.x, tmpMax.y, tmpMin.z));
+		Gizmos.DrawLine(tmpMax, new Vector3(tmpMax.x, tmpMin.y, tmpMax.z));
+		Gizmos.DrawLine(tmpMax, new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
 
-        Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMin.y, tmpMax.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
-        Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
+		Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMin.y, tmpMax.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
+		Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMin.y, tmpMax.z));
 
-        Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMax.y, tmpMin.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
-        Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMax.y, tmpMin.z));
+		Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMax.y, tmpMin.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
+		Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMax.y, tmpMin.z));
 
-        Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
-        Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
-    }
+		Gizmos.DrawLine(new Vector3(tmpMin.x, tmpMin.y, tmpMax.z), new Vector3(tmpMin.x, tmpMax.y, tmpMax.z));
+		Gizmos.DrawLine(new Vector3(tmpMax.x, tmpMax.y, tmpMin.z), new Vector3(tmpMax.x, tmpMin.y, tmpMin.z));
+	}
 
 
 #endif
