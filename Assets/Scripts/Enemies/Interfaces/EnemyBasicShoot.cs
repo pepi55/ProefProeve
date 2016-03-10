dc91c1a2e17c0464796c743538575d57dc6415aa
Nolimet <nolimet@stamdata.nl>
2016-03-10 14:33:33 +0100

    added enemy shoot function (needs to be tested) added bullet pool

---
+++ Assets/Scripts/Enemies/Interfaces/EnemyBasicShoot.cs
@@ -0,0 +1,25 @@
+﻿//Author Jesse Stam
+//Date 10-3-2016
+
+using UnityEngine;
+using System.Collections;
+
+public class EnemyBasicShoot : EnemyBaseInterface
+{
+    System.DateTime LastShot;
+    float Delay = 0.4f;
+
+    public void DoAction(GameObject Entity)
+    {
+        if ((System.DateTime.Now - LastShot).TotalSeconds > Delay)
+        {
+            Delay = Random.Range(0.1f, 4f);
+            LastShot = System.DateTime.Now;
+
+            BaseProjectile p = BulletManager.GetEnemyBullet();
+
+            p.transform.position = Entity.transform.position;
+            p.Reset();
+        }
+    }
+}
---
+++ Assets/Scripts/Projectiles/BaseProjectile.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseProjectile : MonoBehaviour
+{
+    public bool IsAlive { get; private set; }
+    public bool IsRemoved { get; private set; }
+    private bool removeActive;
+
+    new SphereCollider collider;
+    Rigidbody rigibody;
+    void Start()
+    {
+        Reset();
+        collider = GetComponent<SphereCollider>();
+        rigibody = GetComponent<Rigidbody>();
+    }
+
+    public void Reset()
+    {
+        IsAlive = true;
+        IsRemoved = false;
+        gameObject.SetActive(true);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Remove(0.3f);
+    }
+
+    public void Remove(float delay)
+    {
+        StartCoroutine(RemoveDelay(delay));
+    }
+
+    IEnumerator RemoveDelay(float delay)
+    {
+        if (!removeActive)
+        {
+            removeActive = true;
+            collider.enabled = false;
+
+            yield return new WaitForSeconds(delay);
+            removeActive = false;
+            IsRemoved = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
