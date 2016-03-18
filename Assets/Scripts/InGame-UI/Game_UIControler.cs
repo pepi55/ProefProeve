09f24697940cae4590b73f30f8aa4f0c7a48a970
Nolimet <nolimet@stamdata.nl>
2016-03-18 12:41:54 +0100

    added Pause Menu bits

--- Assets/Scripts/InGame-UI/Game_UIControler.cs
+++ Assets/Scripts/InGame-UI/Game_UIControler.cs
@@ -45,4 +45,9 @@ public class Game_UIControler : MonoBehaviour {
         PauseMenu.SetActive(false);
         InGAmeUI.SetActive(true);
     }
+
+    public void ExitToMain()
+    {
+        SceneControler.Load("MainMenu");
+    }
 }
